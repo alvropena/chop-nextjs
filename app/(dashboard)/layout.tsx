f642d665c654e35ffb4b150ca6a0acd051b52e9a
alvropena <me@alvropena.com>
2024-06-17 00:56:23 -0500

    Updating

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -10,6 +10,7 @@ import { usePathname } from "next/navigation";
 import { ModeToggle } from "@/components/mode-toggle";
 import { AvatarDropdownMenu } from "./_components/avatar-dropdown-menu";
 import { NotificationsDropdownMenu } from "./_components/notifications-dropdown-menu";
+import { Badge } from "@/components/ui/badge";
 
 interface NavLinkProps {
     href: string;
@@ -51,12 +52,16 @@ export default function DashboardLayout({
                 <div className="grid min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]">
                     <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
                         <div className="flex h-full max-h-screen flex-col gap-2">
-                            <div className="flex h-[60px] items-center border-b px-6">
+                            <div className="flex h-[60px] items-center border-b px-6 justify-between">
                                 <Link href="#" className="flex items-center gap-2 font-semibold" prefetch={false}>
                                     <GlassesIcon className="h-6 w-6" />
-                                    <span className="">Chop</span>
+                                    <span>Chop</span>
                                 </Link>
+                                <Badge>
+                                    Beta
+                                </Badge>
                             </div>
+
                             <div className="flex-1 overflow-auto py-2">
                                 <nav className="grid items-start px-4 text-sm font-medium">
                                     {navLinks.map(({ href, icon, label }) => (
@@ -78,9 +83,12 @@ export default function DashboardLayout({
                             </div>
                         </div>
                     </div>
-
                     <div className="flex flex-col">
                         <header className="flex h-14 lg:h-[60px] items-center justify-end gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
+
+                            <Button size={"sm"}>
+                                Upgrade
+                            </Button>
                             <NotificationsDropdownMenu />
                             <ModeToggle />
                             <AvatarDropdownMenu />
