18cc68df82d266632aaa4323e0e26a87ea282861
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-06-19 16:24:03 -0600

    deleted env variable in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,6 @@ export default function AuthPage() {
             <Link href={!auth ? `${baseUrl}/Prod/api/v1/login` : `/home`}>
               <Button className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
                 Get Started
-                {process.env.NEXT_PUBLIC_API_BASE_URL}
               </Button>
             </Link>
           </div>
