527ec72994cc9dc0573a637438648c8af1e88e27
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-06-19 16:21:34 -0600

    changed .env to secrets

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function AuthPage() {
           <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
             <Link href={!auth ? `${baseUrl}/Prod/api/v1/login` : `/home`}>
               <Button className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
-                Get Started asdfasdf
+                Get Started
                 {process.env.NEXT_PUBLIC_API_BASE_URL}
               </Button>
             </Link>
