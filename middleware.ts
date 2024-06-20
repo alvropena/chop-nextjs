ed84757de1347dc54950a396f2c1eeefb65ac288
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-06-20 16:02:23 -0600

    auth0 impl

--- middleware.ts
+++ middleware.ts
@@ -1,50 +1,58 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+// const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-const checkAuthentication = async (token: string) => {
-  try {
-    const response = await fetch(`${baseUrl}/Prod/api/v1/auth/verify`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      credentials: "include",
-    });
+// const checkAuthentication = async (token: string) => {
+//   try {
+//     const response = await fetch(`${baseUrl}/Prod/api/v1/auth/verify`, {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       credentials: "include",
+//     });
 
-    if (response.ok) {
-      const data = await response.json();
-      return { accessToken: data, isAuthenticated: true };
-    }
+//     if (response.ok) {
+//       const data = await response.json();
+//       return { accessToken: data, isAuthenticated: true };
+//     }
 
-    console.error("Response status:", response.status, response.statusText);
-    const errorData = await response.json();
-    console.error("Error data:", errorData);
+//     console.error("Response status:", response.status, response.statusText);
+//     const errorData = await response.json();
+//     console.error("Error data:", errorData);
 
-    return { isAuthenticated: false };
-  } catch (error) {
-    console.error("Error checking authentication:", error);
-    return { isAuthenticated: false };
-  }
-};
+//     return { isAuthenticated: false };
+//   } catch (error) {
+//     console.error("Error checking authentication:", error);
+//     return { isAuthenticated: false };
+//   }
+// };
 
-export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value ?? "";
+// export async function middleware(request: NextRequest) {
+//   const token = request.cookies.get("token")?.value ?? "";
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  const { isAuthenticated } = await checkAuthentication(token);
+//   const { isAuthenticated } = await checkAuthentication(token);
 
-  if (!isAuthenticated) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   if (!isAuthenticated) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/home", "/profile", "/history", "/billing", "/settings"],
+// };
+
+import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+
+export default withMiddlewareAuthRequired();
 
 export const config = {
   matcher: ["/home", "/profile", "/history", "/billing", "/settings"],
