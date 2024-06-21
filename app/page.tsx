40e603ab9a53b804e3069da507ab7f1e02824e5e
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-06-20 23:23:38 -0600

    rootPage modified

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,18 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Footer from "@/app/(dashboard)/_components/footer";
 import { checkAuthentication, logoutAndClearLocalStorage } from "@/lib/utils";
+import { getSession } from "@auth0/nextjs-auth0";
+import { useUser } from "@auth0/nextjs-auth0/client";
+export const LogoutButton = () => {
+  return (
+    <Link href={"/api/auth/logout"}>
+      <Button>Sign Out</Button>
+    </Link>
+  );
+};
 
 export default function AuthPage() {
-  const [auth, setAuth] = useState(false);
-  useEffect(() => {
-    // Llamar a la función asincrónica y actualizar el estado basado en su resultado
-    const fetchAuth = async () => {
-      const authState = await checkAuthentication();
-      console.log(authState.isAuthenticated);
-      setAuth(authState.isAuthenticated);
-    };
-    fetchAuth();
-  }, [auth]);
-
-  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const { user } = useUser();
 
   return (
     <div className="flex flex-col min-h-[100dvh]">
@@ -31,19 +29,12 @@ export default function AuthPage() {
           <span className="">Chop</span>
         </Link>
         <div className="ml-auto flex items-center gap-4">
-          {!auth ? (
+          {!user ? (
             <Link href={"/api/auth/login"}>
               <Button>Sign In</Button>
             </Link>
           ) : (
-            <Button
-              onClick={async () => {
-                await logoutAndClearLocalStorage();
-                setAuth(false);
-              }}
-            >
-              Sign Out
-            </Button>
+            <LogoutButton />
           )}
         </div>
       </header>
@@ -56,7 +47,7 @@ export default function AuthPage() {
             The first generative learning platform.
           </p>
           <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
-            <Link href={!auth ? `${baseUrl}/Prod/api/v1/login` : `/home`}>
+            <Link href={!user ? `/api/auth/login` : `/home`}>
               <Button className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
                 Get Started
               </Button>
