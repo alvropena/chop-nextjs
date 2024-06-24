ed241347b87171d3fb8e6764563d9c4dc5cfc90f
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-06-24 13:29:44 -0600

    Feature/#115 setup react forms (#33)
    * added zod for react forms
    
    * profile zod & componentes updated
    
    * callback auth - for develop env

--- app/(dashboard)/_components/date-picker.tsx
+++ app/(dashboard)/_components/date-picker.tsx
@@ -3,29 +3,58 @@
 import * as React from "react";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useController, useFormContext } from "react-hook-form";
 
-export function DatePicker() {
-    const [date, setDate] = React.useState<Date>();
+interface DatePickerProps {
+  name: string;
+}
 
-    return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    variant={"outline"}
-                    className={cn("w-full justify-start text-left font-normal", !date && "text-muted-foreground")}
-                >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? format(date, "PPP") : <span>Pick a date</span>}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-                <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
-            </PopoverContent>
-        </Popover>
-    );
+export function DatePicker({ name }: DatePickerProps) {
+  const { control } = useFormContext();
+  const {
+    field: { onChange, value },
+  } = useController({
+    name,
+    control,
+  });
+
+  const [date, setDate] = React.useState<Date | undefined>(value);
+
+  const handleDateChange = (selectedDate: Date) => {
+    setDate(selectedDate);
+    onChange(selectedDate);
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !date && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onDayClick={handleDateChange}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
 }
