f88198daa315bddaea02dbce7acbfd321aaa24d0
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-06-24 11:46:11 -0600

    added schema store & provider

--- providers/zustand-provider.tsx
+++ providers/zustand-provider.tsx
@@ -4,19 +4,22 @@ import { QuestionStoreProvider } from "./question-store-provider";
 import { ResponsesStoreProvider } from "./responses-store-provider";
 import { PromptStoreProvider } from "./prompt-store-provider";
 import { NotificationsStoreProvider } from "./notifications-store-provider";
+import { SchemaStoreProvider } from "./schema-store-provider";
 type ZustandProviderProps = {
   children: ReactNode;
 };
 export const ZustandProvider = ({ children }: ZustandProviderProps) => {
   return (
-    <NotificationsStoreProvider>
-      <PromptStoreProvider>
-        <ResponsesStoreProvider>
-          <QuestionStoreProvider>
-            <UserStoreProvider>{children}</UserStoreProvider>
-          </QuestionStoreProvider>
-        </ResponsesStoreProvider>
-      </PromptStoreProvider>
-    </NotificationsStoreProvider>
+    <SchemaStoreProvider>
+      <NotificationsStoreProvider>
+        <PromptStoreProvider>
+          <ResponsesStoreProvider>
+            <QuestionStoreProvider>
+              <UserStoreProvider>{children}</UserStoreProvider>
+            </QuestionStoreProvider>
+          </ResponsesStoreProvider>
+        </PromptStoreProvider>
+      </NotificationsStoreProvider>
+    </SchemaStoreProvider>
   );
 };
