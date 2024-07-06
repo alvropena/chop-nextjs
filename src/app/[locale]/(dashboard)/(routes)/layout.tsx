311667a8c92cd761ecf4a9478d8315a87f72f80c
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2024-07-06 13:19:12 -0600

    #121 mobile navigation bar

--- src/app/[locale]/(dashboard)/(routes)/layout.tsx
+++ src/app/[locale]/(dashboard)/(routes)/layout.tsx
@@ -16,31 +16,34 @@ export default function DashboardLayout({
   params: { locale: string }
 }) {
   return (
-    <html>
-      <body>
-        <div className='grid min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]'>
-          <div className='hidden border-r bg-gray-100/40 dark:bg-gray-800/40 lg:block'>
-            <div className='flex h-full max-h-screen flex-col gap-2'>
-              <Navbar locale={locale} />
-            </div>
-          </div>
-          <div className='flex flex-col'>
-            <header className='flex h-14 items-center justify-between gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40 lg:h-[60px]'>
-              <div className='flex items-center'>
-                <MobileSidebar />
-              </div>
-              <div className='flex flex-row items-center gap-4'>
-                {/* <Button>Upgrade</Button> 
-                                <GiveFeedbackDialog />
-                                <NotificationsDropdownMenu /> */}
-                <ThemeSwitch />
-                <AvatarDropdownMenu />
-              </div>
-            </header>
-            {children}
+    <>
+      <div className="grid min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]">
+        {/* Sidebar for large screens */}
+        <div className="hidden lg:block lg:w-72 border-r bg-gray-100/40 dark:bg-gray-800/40">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <Navbar locale={locale} />
           </div>
         </div>
-      </body>
-    </html>
-  )
+        {/* Main content area */}
+        <div className="flex flex-col">
+          {/* Header */}
+          <header className="flex h-14 items-center justify-between gap-4 border-b bg-gray-100/40 px-4 dark:bg-gray-800/40 lg:h-[60px] lg:px-6">
+            <div className="flex flex-col">
+              <MobileSidebar />
+            </div>
+            <div className="flex flex-row items-center gap-4">
+              {/* Uncomment the below lines if these components are needed */}
+              {/* <Button>Upgrade</Button> 
+                  <GiveFeedbackDialog />
+                  <NotificationsDropdownMenu /> */}
+              <ThemeSwitch />
+              <AvatarDropdownMenu />
+            </div>
+          </header>
+          {/* Main children content */}
+          <main className="flex-grow p-4 lg:p-6">{children}</main>
+        </div>
+      </div>
+    </>
+  );
 }
