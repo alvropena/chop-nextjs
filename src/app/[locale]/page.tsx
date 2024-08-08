d2846eb517810e7a058833c2ee1d325cae113ca2
alvropena <me@alvropena.com>
2024-08-08 18:52:24 -0500

    Add Input component and enhance TypingEffect in AuthPage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,7 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { useRouter } from "next/navigation";
 import Loading from "@/app/[locale]/loading";
 import TypingEffect from "@/lib/typing-effect";
+import { Input } from "@/components/ui/input";
 
 export default function AuthPage() {
   const { user, isLoading } = useUser();
@@ -20,7 +21,7 @@ export default function AuthPage() {
   if (!user) {
     return (
       <div className="flex flex-col min-h-[100dvh]">
-        <header className="px-4 lg:px-6 h-14 flex items-center">
+        {/* <header className="px-4 lg:px-6 h-14 flex items-center">
           <Link
             href="#"
             className="flex items-center justify-center"
@@ -33,23 +34,42 @@ export default function AuthPage() {
               <Button>Sign In</Button>
             </Link>
           </div>
-        </header>
+        </header> */}
         <main className="flex-1 flex flex-col justify-center items-center text-center py-12 md:py-24 lg:py-32 border-b">
-          <div className="space-y-4 max-w-3xl">
-            <TypingEffect text="Learn quicker." className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl" />
-            <p className="text-gray-500 text-xl md:text-2xl dark:text-gray-400">
-              The first generative learning platform.
-            </p>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
+          <div className="space-y-6 -w-3xl">
+            <div className="flex flex-col items-center">
+              <div className="flex items-center space-x-2">
+                <p className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">Learn</p>
+                <TypingEffect
+                  texts={[
+                    { word: "soccer", emoji: "⚽️" },
+                    { word: "poker", emoji: "🃏" },
+                    { word: "chemistry", emoji: "🔬" },
+                    { word: "math", emoji: "➗" },
+                    { word: "Italian", emoji: "🇮🇹" },
+                  ]}
+                  className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl"
+                />
+
+              </div>
+              <p className="text-gray-500 text-xl md:text-2xl dark:text-gray-400 mt-2">
+                The first generative learning platform.
+              </p>
+            </div>
+            <div className="flex items-center space-x-2">
+              <Input type="email" placeholder="Email" />
+              <Button>Sign Up</Button>
+            </div>
+          </div>
+          {/* <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
               <Link href={!user ? `/api/auth/login` : `/home`}>
                 <Button>
                   Get Started
                 </Button>
               </Link>
-            </div>
-          </div>
+            </div> */}
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     );
   } else {
