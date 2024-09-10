c0d4d2372bc395d97a069759bb625360e65a33cc
alvropena <me@alvropena.com>
2024-09-09 23:47:11 -0700

    Add user profile components and providers

--- app/[locale]/[username]/page.tsx
+++ app/[locale]/[username]/page.tsx
@@ -1,13 +1,82 @@
 "use client";
 
-import { useParams } from 'next/navigation';
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import { format } from 'date-fns';
+import { useUser } from '../../../hooks/use-user';
+import { LineChart } from 'lucide-react';
+import { PersonIcon } from '@radix-ui/react-icons';
+import { Button } from '../../../components/ui/button';
+import TopicButtons from '../../../components/topic-buttons';
+import { UserProfileHeader } from '../../../components/user-profile/user-header';
+import { UserAchievement } from '../../../components/user-profile/user-achievement';
+import { UserActivity } from '../../../components/user-profile/user-activity';
+import Link from 'next/link';
 
-export default function CommunityPage() {
-  const { username } = useParams();
+export default function ProfilePage() {
+  const { user } = useUser(); // Get global user data from the context
+
+  const pathname = usePathname();
+  const locale = pathname.split('/')[1];
+  const getLocalizedPath = (path: string) => `/${locale}${path}`;
+  const joinedDate = format(new Date(user.created_at), "MMMM yyyy");
+  const lastActiveDate = format(new Date(user.updated_at), "MMM d, yyyy 'at' h:mm a");
+
+  const randomAchievements = user.achievements ? user.achievements.slice(0, 4) : []; // Adjust this as per your data structure
 
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="flex justify-center h-2/3 text-foreground p-4">
+      <div className="flex flex-col space-6 w-full max-w-xl rounded-md">
+
+        {/* Profile Header Component */}
+        <UserProfileHeader
+          username={user.username}
+          name={user.name}
+          bio={user.bio}
+          location={user.location}
+          joinedDate={joinedDate}
+          profilePicture={user.profile_picture}
+          verified={user.verified}
+          getLocalizedPath={getLocalizedPath}
+        />
+
+        <div className='flex flex-row w-full mt-4'>
+          <Link href={getLocalizedPath('/profile/edit')} className="flex-1 mx-4">
+            <Button className="py-2 w-full" variant="default">
+              <PersonIcon className='mr-2 h-4 w-4' />
+              Edit Profile
+            </Button>
+          </Link>
+          <Link href={getLocalizedPath('/profile/analytics')} className="flex-1 mx-4">
+            <Button className="py-2 w-full" variant="default">
+              <LineChart className='mr-2 h-4 w-4' />
+              View Analytics
+            </Button>
+          </Link>
+        </div>
+
+        {/* Profile Topics Component */}
+        <div className="mt-4 w-full">
+          <TopicButtons
+            selectedTopic={user.selectedTopic || ""}
+            handleTopicClick={() => { }}
+            topics={[
+              { id: "geography", label: "🌍 Geography" },
+              { id: "history", label: "📜 History" },
+              { id: "soccer", label: "⚽ Soccer" },
+              { id: "art-history", label: "🖼️ Art History" },
+              { id: "basketball", label: "🏀 Basketball" },
+              { id: "formula1", label: "🏎️ Formula 1" },
+              { id: "music", label: "🎵 Music" },
+            ]}
+            title="Recent"
+            showChevron={false}
+          />
+        </div>
+
+        {/* Profile Activity Component */}
+        <UserActivity activities={user.activities || []} />
+      </div>
     </div>
   );
 }
--- providers/user-provider.tsx
+++ providers/user-provider.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { UserContext } from '../context/user-context';
 import { UserProfileType } from '../types/user/user-profile-type';
+import { UserProfileData } from '../data/user-profile-data'; // Import the UserProfileData
 
 export interface UserProviderProps {
   children: ReactNode;
@@ -11,6 +12,12 @@ export interface UserProviderProps {
 // Provide the user profile context with state and logic
 export const UserProvider = ({ children }: UserProviderProps) => {
   const [user, setUser] = useState<UserProfileType | null>(null);
+
+  useEffect(() => {
+    // Simulate loading the logged-in user (e.g., the first user in UserProfileData)
+    const loggedInUser = UserProfileData.find(profile => profile.username === 'alvropena');
+    setUser(loggedInUser || null);
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
