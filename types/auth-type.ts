7e22fd3bb7d79cd9cd6235011995ec0d5000edad
Vladimir Infante <infantevladimir@gmail.com>
2024-09-11 09:57:32 -0500

    feat: added types for GoogleUser

--- types/auth-type.ts
+++ types/auth-type.ts
@@ -6,6 +6,17 @@ export interface AuthUser {
 
 export type UserId = AuthUser["id"];
 
-export type UserSession = {
+export interface UserSession {
   id: UserId;
-};
+}
+
+export interface GoogleUser {
+  sub: string;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  email: string;
+  email_verified: boolean;
+  locale: string;
+}
