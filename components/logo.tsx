8d8c76a4e518320ea5fdbdf19afc3fce17cfe6c4
Vladimir Infante <infantevladimir@gmail.com>
2024-09-11 22:17:17 -0500

    feat: updated logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -7,27 +7,28 @@ import { useIsMounted } from "../hooks/use-is-mounted";
 interface LogoProps {
     height?: number;
     width?: number;
+    className?: string;
 }
 
-export default function Logo({height = 100, width = 100}:LogoProps) {
+export default function Logo({ className, height = 100, width = 100}:LogoProps) {
     const { theme, systemTheme } = useTheme();
     const isMounted = useIsMounted();
 
     // Delay rendering until mounted to avoid mismatch between server and client
     if (!isMounted || !theme) {
         // Render the placeholder until the theme is determined
-        return <Image height={100} width={100} alt="logo" src={"/logo.svg"} />;
+        return <Image className={className} height={height} width={width} alt="logo" src={"/images/logo.svg"} />;
     }
 
     const getImage = () => {
         if (theme === "system" && systemTheme) {
-            return systemTheme === "dark" ? "/logo.svg" : "/logo_d.svg";
+            return systemTheme === "dark" ? "/images/logo.svg" : "/images/logo_d.svg";
         }
-        return theme === "dark" ? "/logo.svg" : "/logo_d.svg";
+        return theme === "dark" ? "/images/logo.svg" : "/images/logo_d.svg";
     };
 
     // Set the logo image based on the actual theme
     const LogoImage = getImage();
 
-    return <Image height={height} width={width} alt="logo" src={LogoImage} />;
+    return <Image className={className} height={height} width={width} alt="logo" src={LogoImage} />;
 }
