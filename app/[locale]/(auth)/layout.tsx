1ffd20dbe8ba2ce10dfc5db814c4862a2a5c33f1
Vladimir Infante <infantevladimir@gmail.com>
2024-09-13 13:03:55 -0500

    feat: updated auth pages layout

--- app/[locale]/(auth)/layout.tsx
+++ app/[locale]/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import LandingFooter from "@/components/landing/landing-footer";
 import LandingHeader from "@/components/landing/landing-header";
-import Logo from "@/components/logo";
+import { AuthLogoLayout } from "@/components/composites/auth-logo-layout";
 
 export const metadata = {
   title: "Chop",
@@ -14,18 +14,10 @@ export default function LandingLayout({
 }) {
   return (
     <section className="flex flex-col min-h-screen">
+      <LandingHeader />
       <div className="flex-1 flex flex-col items-center justify-center p-6">
         <div className="max-w-6xl w-full space-y-8">
-          <section className="flex justify-between">
-            <div className="w-1/2 flex items-center justify-center h-[inherit]">
-              <Logo height={400} width={400} className="w-[25rem] h-[25rem]" />
-            </div>
-            <div className="flex items-center justify-center py-6">
-              <div className="mx-auto max-w-[22.375rem] space-y-4">
-                {children}
-              </div>
-            </div>
-          </section>
+          <AuthLogoLayout>{children}</AuthLogoLayout>
         </div>
       </div>
       <LandingFooter />
