9be6c1353467d4141fa1a74635ec70e5d4503462
Vladimir Infante <infantevladimir@gmail.com>
2024-09-13 12:56:38 -0500

    feat: added constant for logo url (used in email templates)

--- data/app-data.ts
+++ data/app-data.ts
@@ -3,3 +3,5 @@ export const APPLICATION_NAME = "Chop";
 export const BASE_URL = process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000";
 
 export const AFTER_LOGIN_URL = "/home";
+
+export const LOGO_FOR_EMAIL = process.env.LOGO_FOR_EMAIL;
