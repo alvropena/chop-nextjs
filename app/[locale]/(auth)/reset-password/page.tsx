01e7a6efe5521f4d30a1359905d081d900ce7532
Vladimir Infante <infantevladimir@gmail.com>
2024-09-16 17:53:47 -0500

    feat: now forgot password and reset password forms can be used in small screens

--- app/[locale]/(auth)/reset-password/page.tsx
+++ app/[locale]/(auth)/reset-password/page.tsx
@@ -7,7 +7,7 @@ export default function ResetPasswordPage({
 }) {
   return (
     <>
-      <div className="space-y-3 text-center w-[22.375rem]">
+      <div className="space-y-3 text-center w-[21rem] sm:w-[22.375rem]">
         <h1 className="text-3xl font-bold">Reset password</h1>
       </div>
       <div className="space-y-4">
--- app/[locale]/(auth)/sign-in/forgot-password/page.tsx
+++ app/[locale]/(auth)/sign-in/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ForgotPasswordPage() {
   return (
     <>
-      <div className="space-y-3 text-center w-[22.375rem]">
+      <div className="space-y-3 text-center w-[21rem] sm:w-[22.375rem]">
         <h1 className="text-3xl font-bold">Forgot Password</h1>
         <p className="text-gray-500 dark:text-gray-400">
           Already have your password?{" "}
