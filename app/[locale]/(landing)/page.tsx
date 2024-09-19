f16ec173ca77faa89fe20bc4e9b96ec6085cde34
Vladimir Infante <infantevladimir@gmail.com>
2024-09-19 14:15:30 -0500

    feat: implemented contact section in landing page

--- app/[locale]/(landing)/page.tsx
+++ app/[locale]/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "@/components/sections/hero-section";
 import { AboutSection } from "@/components/sections/about-section";
 import { PricingSection } from "@/components/sections/pricing-section";
 import { BlogSection } from "@/components/sections/blog-section";
+import { ContactSection } from "@/components/sections/contact-section";
 
 export default function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export default function LandingPage() {
       <AboutSection />
       <PricingSection />
       <BlogSection />
+      <ContactSection />
     </main>
   );
 }
