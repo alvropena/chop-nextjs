c8fb1cc371e3f941b266f408912f2595bff10bdc
Vladimir Infante <infantevladimir@gmail.com>
2024-09-19 13:02:48 -0500

    feat: updated styles in blog post card

--- components/prismic/post-card.tsx
+++ components/prismic/post-card.tsx
@@ -13,18 +13,20 @@ export const PostCard = ({
   return (
     <PrismicLink
       document={post}
-      className="max-w-80 h-[23.75rem] rounded-xl overflow-hidden space-y-4 shadow-lg shadow-muted"
+      className="w-80 h-[25rem] rounded-xl overflow-hidden space-y-4 shadow-lg shadow-muted"
     >
       <div className="max-h-56 overflow-hidden">
-        <PrismicNextImage
-          field={data.featured_image}
-          sizes="100vw"
-          className="w-full max-w-80 max-h-60 rounded-xl object-fill"
-        />
+        <div className="relative w-80 h-60">
+          <PrismicNextImage
+            field={data.card_image}
+            sizes="100vw"
+            className="w-full max-w-80 max-h-60 rounded-xl object-fill"
+          />
+        </div>
       </div>
-      <div className="flex flex-col gap-3 px-2 pb-3">
+      <div className="flex flex-col gap-3 px-3 pb-3 text-ellipsis">
         <div className="flex flex-col gap-2">
-          <p className="text-sm opacity-75 text-gray-300 border-b-2 w-min pb-2">
+          <p className="text-sm opacity-75 text-muted-foreground border-b-2 w-min pb-2">
             {new Date(data?.publication_date || "").toLocaleDateString()}
           </p>
           <div className="hover:opacity-75 duration-300 ease-in-out transition-all">
@@ -33,7 +35,7 @@ export const PostCard = ({
             </h2>
           </div>
         </div>
-        <div className="text-sm">
+        <div className="text-sm line-clamp-3">
           <RichText field={data.description} />
         </div>
       </div>
