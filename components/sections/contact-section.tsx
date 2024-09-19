d271dc825bce027c9afb1e747646fe9d54363953
Vladimir Infante <infantevladimir@gmail.com>
2024-09-19 14:15:06 -0500

    feat: added contact section for landing page

---
+++ components/sections/contact-section.tsx
@@ -0,0 +1,18 @@
+import { useTranslations } from "next-intl";
+import { ContactForm } from "@/components/composites/contact-form";
+
+export function ContactSection() {
+  const t = useTranslations("ContactPage");
+
+  return (
+    <section id="contact" className="pb-10 mx-auto max-w-6xl w-5/6">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+          {t("contact")}
+        </h1>
+        <p className="mt-4 text-muted-foreground">{t("contactDescription")}</p>
+      </div>
+      <ContactForm />
+    </section>
+  );
+}
