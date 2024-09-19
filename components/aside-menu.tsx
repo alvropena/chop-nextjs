a0a47aa934b6ae26117d9aa41b25436cfc99826e
Vladimir Infante <infantevladimir@gmail.com>
2024-09-19 18:39:09 -0500

    feat: fixed bug in aside menu, added overflow hidden

--- components/aside-menu.tsx
+++ components/aside-menu.tsx
@@ -30,7 +30,7 @@ export default function AsideMenu({ }) {
 
   }
   const baseClasses =
-    "relative inline-flex items-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md";
+    "relative inline-flex w-full items-center text-sm px-3 font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-9 rounded-md";
   const activeClasses = "bg-primary text-primary-foreground";
   const hoverClasses = "hover:bg-secondary hover:text-secondary-foreground";
   const defaultClasses = "bg-transparent text-foreground";
@@ -40,7 +40,7 @@ export default function AsideMenu({ }) {
     baseClasses,
     pathname === '/search' ? activeClasses : defaultClasses,
     pathname !== '/search' && hoverClasses,
-    isCollapsed ? "justify-center w-full" : "justify-start px-3 w-full"
+    isCollapsed ? "justify-center" : "justify-start"
   );
 
   useEffect(() => {
@@ -50,7 +50,7 @@ export default function AsideMenu({ }) {
   }, [isCollapsed]);
 
   return (
-    <aside className={cn("flex-col border-r bg-background p-4 transition-all duration-300", isCollapsed ? "w-16" : "w-56", "hidden sm:flex")}>
+    <aside className={cn("flex-col border-r bg-background p-4 transition-all duration-300 overflow-hidden", isCollapsed ? "w-16" : "w-56", "hidden sm:flex")}>
       <div className={cn("flex items-center", isCollapsed ? "justify-center" : "justify-between mb-4")}>
         {!isCollapsed &&
           <Link href="/">
@@ -66,8 +66,8 @@ export default function AsideMenu({ }) {
         <Dialog>
           <DialogTrigger asChild>
             <button className={searchButtonClasses}>
-              <SearchIcon className="h-5 w-5" />
-              {!isCollapsed && <span className="ml-2">{t("search")}</span>}
+              <SearchIcon className="h-5 w-5 shrink-0" />
+               <span className={cn("ml-2", isCollapsed && 'hidden')}>{t("search")}</span>
             </button>
           </DialogTrigger>
           <DialogContent>
