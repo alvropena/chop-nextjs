91b3a28b9318a31a0d325e50dac9ba459bb4516f
Vladimir Infante <infantevladimir@gmail.com>
2024-09-20 17:26:04 -0500

    feat: updated user page when user not found

--- app/[locale]/[username]/page.tsx
+++ app/[locale]/[username]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { UserProfileData } from "@/data/user-profile-data";
 import { UserProfileHeader } from "@/components/user-profile/user-header";
 
@@ -11,15 +12,20 @@ export default function UserProfilePage({
   if (!userData) {
     return (
       <div className="flex flex-col items-center py-8">
-        <h1 className="text-2xl font-bold">User not found</h1>
+        <h1 className="text-3xl font-bold mb-10">
+          Sorry, this page isn't available.
+        </h1>
+        <h1 className="text-lg font-semibold">
+          The link you followed maybe broken, or the page may have been removed.{" "}
+          <span className="text-blue-500 hover:text-blue-400 duration-300">
+            <Link href="/">Go back to Chop.</Link>
+          </span>
+        </h1>
       </div>
     );
   }
   return (
     <div className="flex flex-col items-center py-8">
-      {/* <h1 className="text-2xl font-bold">
-        This is the user profile of: {user}
-      </h1> */}
       <UserProfileHeader {...userData} />
     </div>
   );
