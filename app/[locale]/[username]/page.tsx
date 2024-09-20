ea40b6bfb2341a4d6138641efe2cddffbb08d8bb
Vladimir Infante <infantevladimir@gmail.com>
2024-09-20 16:56:18 -0500

    feat: updated user profile page, now can be rendered in the server

--- app/[locale]/[username]/page.tsx
+++ app/[locale]/[username]/page.tsx
@@ -1,15 +1,26 @@
-"use client";
+import { UserProfileData } from "@/data/user-profile-data";
+import { UserProfileHeader } from "@/components/user-profile/user-header";
 
-import { useParams } from "next/navigation";
-
-export default function UserProfilePage({ params }: { params: { username: string } }) {
-  const { username: urlUsername } = useParams(); // Extract the username from the URL
-
+export default function UserProfilePage({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = params.username;
+  const userData = UserProfileData.find(({ username }) => username === user);
+  if (!userData) {
+    return (
+      <div className="flex flex-col items-center py-8">
+        <h1 className="text-2xl font-bold">User not found</h1>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col items-center py-8">
-      <h1 className="text-2xl font-bold">
-        This is the user profile of: {urlUsername}
-      </h1>
+      {/* <h1 className="text-2xl font-bold">
+        This is the user profile of: {user}
+      </h1> */}
+      <UserProfileHeader {...userData} />
     </div>
   );
 }
