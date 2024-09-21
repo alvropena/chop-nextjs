ad17ef43027a6264529048815b21885bf8ca79f4
Vladimir Infante <infantevladimir@gmail.com>
2024-09-20 20:14:48 -0500

    feat: updated base card and multiple question selector, now show feedback on correct/wrong answers

--- components/cards/base-card.tsx
+++ components/cards/base-card.tsx
@@ -1,14 +1,64 @@
 import React, { useState } from "react";
+import Image from "next/image";
 import { Progress } from "../ui/progress";
 import { CardTitle, CardDescription, Card } from "../ui/card";
 import { Button } from "../ui/button";
-import type { BaseCardType } from "@/types/card/base-card-type";
+import type { BaseCardType, QuestionType } from "@/types/card/base-card-type";
 import MultipleChoiceQuestion from "./question/multiple-question";
 import FillInTheBlankQuestion from "./question/fill-in-the-blank-question";
 import MatchingPairsQuestion from "./question/matching-pairs-question";
 import SpeakingQuestion from "./question/speaking-question";
 import WritingQuestion from "./question/write-question";
 import CardContent from "./content/card-content";
+import { useCardStore } from "@/stores/use-card-store"; // a
+import { CircleX } from "lucide-react";
+
+const FeedBack = () => {
+  const isCorrect = useCardStore((state) => state.isCorrect);
+  return isCorrect ? (
+    <div className="text-center">
+      <div className="text-green-600 font-bold text-3xl">Congratulations!</div>
+      <div className="relative">
+        <Image
+          src="/images/celebrate.svg"
+          alt="Congrats!"
+          width={240}
+          height={280}
+          className="mx-auto"
+        />
+      </div>
+      <div className="text-green-600 font-bold text-xl">
+        Your answer is correct.
+      </div>
+    </div>
+  ) : (
+    <div className="text-center py-4">
+      <CircleX className="text-red-600 mx-auto mb-7" size={100} />
+      <div className="text-red-600 font-bold text-lg">
+        Oops! That&apos;s not the correct answer.
+      </div>
+    </div>
+  );
+};
+
+const Question = ({ question }: { question: QuestionType }) => {
+  if (!question) return null;
+
+  switch (question.type) {
+    case "multiple-choice":
+      return <MultipleChoiceQuestion {...question} />;
+    case "fill-in-the-blank":
+      return <FillInTheBlankQuestion {...question} />;
+    case "matching-pairs":
+      return <MatchingPairsQuestion {...question} />;
+    case "speaking":
+      return <SpeakingQuestion {...question} />;
+    case "writing":
+      return <WritingQuestion {...question} />;
+    default:
+      return null;
+  }
+};
 
 const BaseCard: React.FC<BaseCardType> = ({
   title,
@@ -16,23 +66,24 @@ const BaseCard: React.FC<BaseCardType> = ({
   question,
   progress,
 }) => {
-  const [showFeedback, setShowFeedback] = useState(false);
-  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
   const [currentProgress, setCurrentProgress] = useState(progress ?? 0);
+  const showFeedback = useCardStore((state) => state.showFeedback);
+  const setIsCorrect = useCardStore((state) => state.setIsCorrect);
+  const setShowFeedback = useCardStore((state) => state.setShowFeedback);
 
   const checkAnswer = () => {
     if (
       question.type === "multiple-choice" ||
       question.type === "fill-in-the-blank"
     ) {
-      if (
-        question.correctAnswer === "Paris" ||
-        question.correctAnswer === "8"
-      ) {
-        setIsCorrect(true);
-      } else {
-        setIsCorrect(false);
-      }
+      // if (
+      //   question.correctAnswer === "Paris" ||
+      //   question.correctAnswer === "8"
+      // ) {
+      //   setIsCorrect(true);
+      // } else {
+      //   setIsCorrect(false);
+      // }
     } else if (question.type === "matching-pairs") {
       setIsCorrect(true); // Example placeholder logic
     } else if (question.type === "speaking") {
@@ -47,37 +98,6 @@ const BaseCard: React.FC<BaseCardType> = ({
 
   const handleContinue = () => {
     setShowFeedback(false);
-  };
-
-  const renderQuestion = () => {
-    if (!question) return null;
-
-    switch (question.type) {
-      case "multiple-choice":
-        return <MultipleChoiceQuestion {...question} />;
-      case "fill-in-the-blank":
-        return <FillInTheBlankQuestion {...question} />;
-      case "matching-pairs":
-        return <MatchingPairsQuestion {...question} />;
-      case "speaking":
-        return <SpeakingQuestion {...question} />;
-      case "writing":
-        return <WritingQuestion {...question} />;
-      default:
-        return null;
-    }
-  };
-
-  const renderFeedback = () => {
-    return isCorrect ? (
-      <div className="text-green-600 font-bold">
-        Congratulations! Your answer is correct.
-      </div>
-    ) : (
-      <div className="text-red-600 font-bold">
-        Oops! That&apos;s not the correct answer.
-      </div>
-    );
   };
 
   return (
@@ -96,7 +116,9 @@ const BaseCard: React.FC<BaseCardType> = ({
                 />
               </CardDescription>
             </div>
-            <div className="mt-4">{renderQuestion()}</div>
+            <div className="mt-4">
+              <Question question={question} />
+            </div>
             <div>
               <Button onClick={checkAnswer} className="mt-4 w-full">
                 Check
@@ -105,7 +127,7 @@ const BaseCard: React.FC<BaseCardType> = ({
           </>
         ) : (
           <>
-            {renderFeedback()}
+            <FeedBack />
             <Button onClick={handleContinue} className="mt-4 w-full">
               Continue
             </Button>
--- components/cards/question/multiple-question.tsx
+++ components/cards/question/multiple-question.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { Button } from "../../ui/button";
 import type { MultipleChoiceQuestionType } from "@/types/card/question/multiple-choice-question-type";
+import { useCardStore } from "@/stores/use-card-store";
 
 const MultipleChoiceQuestion: React.FC<MultipleChoiceQuestionType> = ({
   options,
   correctAnswer,
 }) => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const setIsCorrect = useCardStore((state) => state.setIsCorrect);
 
   const handleOptionClick = (option: string) => {
     setSelectedOption(option);
+    setIsCorrect(option === correctAnswer);
   };
 
   return (
